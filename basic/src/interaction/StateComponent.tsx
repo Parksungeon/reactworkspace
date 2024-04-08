827bd29f38759c44c7a1d11f0f2e72d8fd50ea75
parksungeon <un109101@naver.com>
2024-04-08 10:12:04 +0900

    2024-04-08 10:12

--- basic/src/interaction/StateComponent.tsx
+++ basic/src/interaction/StateComponent.tsx
@@ -6,6 +6,10 @@ import React from 'react'
 // - 컴포넌트는 독립적인 상태를 가질 수 있음
 // - 생태가 변겨오디면 컴포넌트가 리렌더링 됨
 export default function StateComponent() {
+
+    // status 선언 :
+    // - userState 함수로 함수를 선언할 수 있음
+
   return (
     <div>StateComponent</div>
   )
