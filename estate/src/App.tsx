9cb0691240c1aec0a4a5a72ebe7ca07edeb7d408
parksungeon <un109101@naver.com>
2024-04-11 12:59:03 +0900

    2024-04-11 12:58

--- estate/src/App.tsx
+++ estate/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Authentication from 'views/Authenticstion';
+import Authentication from 'views/Authentication';
 
 function App() {
   return (
